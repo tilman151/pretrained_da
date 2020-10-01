5b85a2637132a71c74c2197804bdcb40b00576eb
Tilman Krokotsch <tilman.krokotsch@iav.de>
2020-10-01 14:56:48 +0200

    Add CLI support for multiple percent_broken.

--- src/run.py
+++ src/run.py
@@ -1,3 +1,5 @@
+import random
+
 import pytorch_lightning as pl
 from pytorch_lightning import loggers
 
@@ -9,7 +11,11 @@ def run(percent_broken, seed):
     pl.trainer.seed_everything(seed)
     tf_logger = loggers.TensorBoardLogger('./results', name=f'percent_broken_{percent_broken:.0%}')
     trainer = pl.Trainer(gpus=1, max_epochs=200, logger=tf_logger, deterministic=True)
-    data = datasets.DomainAdaptionDataModule(fd_source=3, fd_target=1, batch_size=512, window_size=30, percent_broken=percent_broken)
+    data = datasets.DomainAdaptionDataModule(fd_source=3,
+                                             fd_target=1,
+                                             batch_size=512,
+                                             window_size=30,
+                                             percent_broken=percent_broken)
     model = lightning.AdaptiveAE(in_channels=14,
                                  seq_len=30,
                                  num_layers=4,
@@ -26,9 +32,19 @@ def run(percent_broken, seed):
     trainer.test(datamodule=data)
 
 
-if __name__ == '__main__':
-    broken = [1.0, 0.8, 0.6, 0.4, 0.2]
-    seeds = [345, 754, 568, 290, 914]
+def run_multiple(broken):
+    broken = broken if opt.broken is not None else [1.0]
+    random.seed(42)
+    seeds = [random.randint(0, 9999999) for _ in broken]
     for b in broken:
         for s in seeds:
             run(b, s)
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Run domain adaption experiment')
+    parser.add_argument('-b', '--broken', nargs='*', type=float, help='percent broken to use')
+    opt = parser.parse_args()
+
+    run_multiple(opt.broken)
