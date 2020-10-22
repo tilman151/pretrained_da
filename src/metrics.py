2d6dbb843922754aed02c2e322003fa401afb870
Tilman Krokotsch <tilman.krokotsch@iav.de>
2020-10-22 14:33:20 +0200

    Make metric pretty.

--- src/metrics.py
+++ src/metrics.py
@@ -21,6 +21,13 @@ class EmbeddingViz(pl.metrics.Metric):
         self.rul_cm = plt.get_cmap('viridis')
 
     def update(self, embeddings, labels, ruls):
+        """
+        Add embedding, labels and RUL to the metric.
+
+        :param embeddings: hidden layer embedding of the data points
+        :param labels: domain labels with 0 being source and 1 being target
+        :param ruls: Remaining Useful Lifetime values of the data points
+        """
         start = self.sample_counter
         end = self.sample_counter + embeddings.shape[0]
         self.sample_counter = end
@@ -30,6 +37,7 @@ class EmbeddingViz(pl.metrics.Metric):
         self.ruls[start:end] = ruls
 
     def compute(self):
+        """Compute UMAP and plot points to 2d scatter plot."""
         logged_embeddings = self.embeddings[:self.sample_counter].detach().cpu().numpy()
         logged_labels = self.labels[:self.sample_counter].detach().cpu().int()
         logged_ruls = self.ruls[:self.sample_counter].detach().cpu()
@@ -38,12 +46,12 @@ class EmbeddingViz(pl.metrics.Metric):
         fig, (ax_class, ax_rul) = plt.subplots(1, 2, sharex='all', sharey='all', figsize=(20, 10))
 
         class_colors = [self.class_cm.colors[c] for c in logged_labels]
-        scatter_class = ax_class.scatter(viz_embeddings[:, 0], viz_embeddings[:, 1],
-                                         c=class_colors, alpha=0.4, edgecolors='none')
+        ax_class.scatter(viz_embeddings[:, 0], viz_embeddings[:, 1],
+                         c=class_colors, alpha=0.4, edgecolors='none', s=[4])
         ax_class.legend(['Source', 'Target'], loc='lower left')
 
         ax_rul.scatter(viz_embeddings[:, 0], viz_embeddings[:, 1],
-                       c=logged_ruls, alpha=0.4, edgecolors='none')
+                       c=logged_ruls, alpha=0.4, edgecolors='none', s=[4])
         color_bar = plt.cm.ScalarMappable(mplcolors.Normalize(logged_ruls.min(), logged_ruls.max()), self.rul_cm)
         color_bar.set_array(logged_ruls)
         plt.colorbar(color_bar)
