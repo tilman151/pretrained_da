3986a18f31c6bbd268dd1a01a5fadfa1cca2e4ba
Tilman Krokotsch <tilman.krokotsch@iav.de>
2020-10-31 10:29:25 +0100

    Add run scripts for all transfers.

---
+++ src/run_transfer34.sh
@@ -0,0 +1,7 @@
+python run.py --source 3 --target 1 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --recon_tradeoff 0.0 -r 10 --gpu 1
+python run.py --source 3 --target 2 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --recon_tradeoff 0.0 -r 10 --gpu 1
+python run.py --source 3 --target 4 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --recon_tradeoff 0.0 -r 10 --gpu 1
+
+python run.py --source 4 --target 1 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --recon_tradeoff 0.0 -r 10 --gpu 1
+python run.py --source 4 --target 2 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --recon_tradeoff 0.0 -r 10 --gpu 1
+python run.py --source 4 --target 3 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --recon_tradeoff 0.0 -r 10 --gpu 1
