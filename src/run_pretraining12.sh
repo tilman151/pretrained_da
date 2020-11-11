61c682078b8c940f2b5555756b08301246a6e062
Tilman Krokotsch <tilman.krokotsch@iav.de>
2020-11-11 16:46:03 +0100

    Add scripts to start pretraining.

---
+++ src/run_pretraining12.sh
@@ -0,0 +1,3 @@
+python run_pretraining.py --source 1 --target 2 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 0.01 -r 10 --gpu 0
+python run_pretraining.py --source 1 --target 3 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 0.01 -r 10 --gpu 0
+python run_pretraining.py --source 1 --target 4 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 0.01 -r 10 --gpu 0
