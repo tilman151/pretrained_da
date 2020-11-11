1f02aec6b28aa2377410b92449934588fb91df6f
Tilman Krokotsch <tilman.krokotsch@iav.de>
2020-11-11 16:42:26 +0100

    Remove deprecated CLI argument from scripts.

--- src/run_transfer12.sh
+++ src/run_transfer12.sh
@@ -1,7 +1,7 @@
-python run.py --source 1 --target 2 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --recon_tradeoff 0.0 -r 10 --gpu 0
-python run.py --source 1 --target 3 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --recon_tradeoff 0.0 -r 10 --gpu 0
-python run.py --source 1 --target 4 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --recon_tradeoff 0.0 -r 10 --gpu 0
+python run.py --source 1 --target 2 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 -r 10 --gpu 0
+python run.py --source 1 --target 3 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 -r 10 --gpu 0
+python run.py --source 1 --target 4 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 -r 10 --gpu 0
 
-python run.py --source 2 --target 1 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --recon_tradeoff 0.0 -r 10 --gpu 0
-python run.py --source 2 --target 3 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --recon_tradeoff 0.0 -r 10 --gpu 0
-python run.py --source 2 --target 4 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --recon_tradeoff 0.0 -r 10 --gpu 0
+python run.py --source 2 --target 1 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 -r 10 --gpu 0
+python run.py --source 2 --target 3 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 -r 10 --gpu 0
+python run.py --source 2 --target 4 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 -r 10 --gpu 0
