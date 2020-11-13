bace91373ebb4bf8c21ba22edd1ff2ebf93b7bd7
Tilman Krokotsch <tilman.krokotsch@iav.de>
2020-11-13 17:44:50 +0100

    Add run scripts for combined pre-training plus adaption experiments.

---
+++ src/run_complete1.sh
@@ -0,0 +1,3 @@
+python run_complete.py --datasets 1 2 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --gpu 0
+python run_complete.py --datasets 1 3 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --gpu 0
+python run_complete.py --datasets 1 4 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --gpu 0
---
+++ src/run_complete2.sh
@@ -0,0 +1,3 @@
+python run_complete.py --datasets 2 3 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --gpu 1
+python run_complete.py --datasets 2 4 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --gpu 1
+python run_complete.py --datasets 3 4 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 1.0 --gpu 1
