52d64620429d7d4e52fe0f92adcf51fec390bdc3
Tilman Krokotsch <tilman.krokotsch@iav.de>
2020-11-20 09:24:16 +0100

    Make it possible to load pre-trained domain disc, too.

--- src/lightning/mixins.py
+++ src/lightning/mixins.py
@@ -7,10 +7,20 @@ class DataHparamsMixin:
 
 
 class LoadEncoderMixin:
-    def load_encoder(self, checkpoint_path):
+    def load_encoder(self, checkpoint_path, load_disc=False):
         checkpoint = torch.load(checkpoint_path, map_location='cpu')
-        encoder_state = {n.replace('encoder.', ''): weight
-                         for n, weight in checkpoint['state_dict'].items()
-                         if n.startswith('encoder')}
+
+        encoder_state = self._extract_state_dict(checkpoint, model='encoder')
         self.encoder.load_state_dict(encoder_state)
+        if load_disc:
+            disc_state = self._extract_state_dict(checkpoint, model='domain_disc')
+            self.domain_disc.load_state_dict(disc_state)
+
         self.hparams['pretrained_checkpoint'] = checkpoint_path
+
+    def _extract_state_dict(self, checkpoint, model):
+        encoder_state = {n.replace(model + '.', ''): weight
+                         for n, weight in checkpoint['state_dict'].items()
+                         if n.startswith(model)}
+
+        return encoder_state
