407b4885b2659b05a72851b77e837002eb7b7742
Tilman Krokotsch <tilman.krokotsch@iav.de>
2020-12-04 10:39:30 +0100

    Delete unused run scripts.

--- src/run_pretraining34.sh
+++
@@ -1,3 +0,0 @@
-python run_pretraining.py --source 2 --target 3 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 0.01 -r 10 --gpu 1
-python run_pretraining.py --source 2 --target 4 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 0.01 -r 10 --gpu 1
-python run_pretraining.py --source 3 --target 4 -b 0.2 0.4 0.6 0.8 1.0 --domain_tradeoff 0.01 -r 10 --gpu 1
