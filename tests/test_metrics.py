6f30925dcf37b570cffad51df65b34856c9208e9
Tilman Krokotsch <tilman.krokotsch@iav.de>
2020-12-04 18:38:32 +0100

    Add metric test.

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -33,3 +33,22 @@ class TestEmbeddingViz(unittest.TestCase):
 
         self.assertIsInstance(fig, plt.Figure)
         self.assertListEqual([20., 10.], list(fig.get_size_inches()))
+
+
+class TestRULScore(unittest.TestCase):
+    def setUp(self):
+        self.metric = metrics.RULScore()
+
+    def test_negative(self):
+        inputs = torch.ones(2)
+        targets = torch.ones(2) * 2
+        actual_score = self.metric(inputs, targets)
+        expected_score = torch.sum(torch.exp((inputs - targets) / self.metric.neg_factor) - 1)
+        self.assertEqual(expected_score, actual_score)
+
+    def test_positive(self):
+        inputs = torch.ones(2) * 2
+        targets = torch.ones(2)
+        actual_score = self.metric(inputs, targets)
+        expected_score = torch.sum(torch.exp((inputs - targets) / self.metric.pos_factor) - 1)
+        self.assertEqual(expected_score, actual_score)
