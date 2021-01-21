7bbcd5f1d24365debc0d21eb1672ddb07669ed98
Tilman Krokotsch <tilman.krokotsch@iav.de>
2021-01-21 16:42:51 +0100

    Fix bug in run scripts.

--- src/run_transfer34.sh
+++ src/run_transfer34.sh
@@ -1,7 +1,7 @@
-python run.py --source 3 --target 1 -b 0.2 0.4 0.6 0.8 1.0 -r 10 --gpu 1 --config ../hyperopt/best_configs/transfer_hyperopt_three\&one@0.80.json
-python run.py --source 3 --target 2 -b 0.2 0.4 0.6 0.8 1.0 -r 10 --gpu 1 --config ../hyperopt/best_configs/transfer_hyperopt_three\&two@0.80.json
-python run.py --source 3 --target 4 -b 0.2 0.4 0.6 0.8 1.0 -r 10 --gpu 1 --config ../hyperopt/best_configs/transfer_hyperopt_three\&four@0.80.json
+python run_dann.py --source 3 --target 1 -b 0.2 0.4 0.6 0.8 1.0 -r 10 --gpu 1 --config ../hyperopt/best_configs/transfer_hyperopt_three\&one@0.80.json
+python run_dann.py --source 3 --target 2 -b 0.2 0.4 0.6 0.8 1.0 -r 10 --gpu 1 --config ../hyperopt/best_configs/transfer_hyperopt_three\&two@0.80.json
+python run_dann.py --source 3 --target 4 -b 0.2 0.4 0.6 0.8 1.0 -r 10 --gpu 1 --config ../hyperopt/best_configs/transfer_hyperopt_three\&four@0.80.json
 
-python run.py --source 4 --target 1 -b 0.2 0.4 0.6 0.8 1.0 -r 10 --gpu 1 --config ../hyperopt/best_configs/transfer_hyperopt_four\&one@0.80.json
-python run.py --source 4 --target 2 -b 0.2 0.4 0.6 0.8 1.0 -r 10 --gpu 1 --config ../hyperopt/best_configs/transfer_hyperopt_four\&two@0.80.json
-python run.py --source 4 --target 3 -b 0.2 0.4 0.6 0.8 1.0 -r 10 --gpu 1 --config ../hyperopt/best_configs/transfer_hyperopt_four\&three@0.80.json
+python run_dann.py --source 4 --target 1 -b 0.2 0.4 0.6 0.8 1.0 -r 10 --gpu 1 --config ../hyperopt/best_configs/transfer_hyperopt_four\&one@0.80.json
+python run_dann.py --source 4 --target 2 -b 0.2 0.4 0.6 0.8 1.0 -r 10 --gpu 1 --config ../hyperopt/best_configs/transfer_hyperopt_four\&two@0.80.json
+python run_dann.py --source 4 --target 3 -b 0.2 0.4 0.6 0.8 1.0 -r 10 --gpu 1 --config ../hyperopt/best_configs/transfer_hyperopt_four\&three@0.80.json
