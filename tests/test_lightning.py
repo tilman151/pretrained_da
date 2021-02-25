c624709736784d28eb2b0ee76e960e8dcbd96f61
Tilman Krokotsch <tilman.krokotsch@iav.de>
2021-02-25 09:30:01 +0100

    Fix lightning tests.

--- tests/test_lightning.py
+++ tests/test_lightning.py
@@ -9,18 +9,20 @@ from lightning import baseline, dann, pretraining
 class TestDAAN(unittest.TestCase):
     def setUp(self):
         self.trade_off = 0.5
-        self.net = dann.DANN(in_channels=14,
-                             seq_len=30,
-                             num_layers=4,
-                             kernel_size=3,
-                             base_filters=16,
-                             latent_dim=64,
-                             dropout=0.1,
-                             domain_trade_off=self.trade_off,
-                             domain_disc_dim=32,
-                             num_disc_layers=2,
-                             optim_type='adam',
-                             lr=0.01)
+        self.net = dann.DANN(
+            in_channels=14,
+            seq_len=30,
+            num_layers=4,
+            kernel_size=3,
+            base_filters=16,
+            latent_dim=64,
+            dropout=0.1,
+            domain_trade_off=self.trade_off,
+            domain_disc_dim=32,
+            num_disc_layers=2,
+            optim_type="adam",
+            lr=0.01,
+        )
 
     @torch.no_grad()
     def test_encoder(self):
@@ -73,8 +75,12 @@ class TestDAAN(unittest.TestCase):
     def test_all_parameters_updated(self):
         optim = torch.optim.SGD(self.net.parameters(), lr=0.1)
 
-        loss, *_ = self.net._train(torch.randn(16, 14, 30), torch.ones(16),
-                                   torch.randn(16, 14, 30), torch.ones(32))
+        loss, *_ = self.net._train(
+            torch.randn(16, 14, 30),
+            torch.ones(16),
+            torch.randn(16, 14, 30),
+            torch.ones(32),
+        )
         loss.backward()
         optim.step()
 
@@ -82,7 +88,7 @@ class TestDAAN(unittest.TestCase):
             if param.requires_grad:
                 with self.subTest(name=param_name):
                     self.assertIsNotNone(param.grad)
-                    self.assertNotEqual(0., torch.sum(param.grad ** 2))
+                    self.assertNotEqual(0.0, torch.sum(param.grad ** 2))
 
     @torch.no_grad()
     def test_train_metrics(self):
@@ -90,11 +96,14 @@ class TestDAAN(unittest.TestCase):
         target = torch.zeros(16, 14, 30)
         source = torch.zeros(16, 14, 30)
         target_labels = torch.ones(16)
-        domain_labels = torch.cat([torch.zeros_like(target_labels),
-                                   torch.ones_like(target_labels)])
+        domain_labels = torch.cat(
+            [torch.zeros_like(target_labels), torch.ones_like(target_labels)]
+        )
 
         expected_prediction = self.net.regressor(self.net.encoder(target))
-        expected_loss = torch.sqrt(criterion(expected_prediction.squeeze(), target_labels))
+        expected_loss = torch.sqrt(
+            criterion(expected_prediction.squeeze(), target_labels)
+        )
         _, actual_loss, _ = self.net._train(target, target_labels, source, domain_labels)
 
         self.assertEqual(expected_loss, actual_loss)
@@ -104,34 +113,38 @@ class TestDAAN(unittest.TestCase):
             inputs = torch.randn(10, 14, 30)
             outputs = self.net.encoder(inputs)
             for sample in outputs:
-                self.assertNotEqual(1., torch.norm(sample, p=2))
+                self.assertNotEqual(1.0, torch.norm(sample, p=2))
 
         with self.subTest(norm=True):
             self.net.encoder.norm_outputs = True
             outputs = self.net.encoder(inputs)
             for sample in outputs:
-                self.assertAlmostEqual(1., torch.norm(sample, p=2).item(), places=5)
+                self.assertAlmostEqual(1.0, torch.norm(sample, p=2).item(), places=5)
 
-    @mock.patch('torch.load')
-    @mock.patch('lightning.mixins.LoadEncoderMixin._extract_state_dict')
-    @mock.patch('torch.nn.Module.load_state_dict')
-    def test_feature_norm_on_transferred_encoder(self, mock_load_state_dict, mock_extract_state_dict, mock_load):
+    @mock.patch("torch.load")
+    @mock.patch("lightning.mixins.LoadEncoderMixin._extract_state_dict")
+    @mock.patch("torch.nn.Module.load_state_dict")
+    def test_feature_norm_on_transferred_encoder(
+        self, mock_load_state_dict, mock_extract_state_dict, mock_load
+    ):
         self.assertFalse(self.net.encoder.norm_outputs)
-        self.net.load_encoder('bogus', load_disc=False)
+        self.net.load_encoder("bogus", load_disc=False)
         self.assertTrue(self.net.encoder.norm_outputs)
 
 
 class TestBaseline(unittest.TestCase):
     def setUp(self):
         self.trade_off = 0.5
-        self.net = baseline.Baseline(in_channels=14,
-                                     seq_len=30,
-                                     num_layers=4,
-                                     kernel_size=3,
-                                     base_filters=16,
-                                     latent_dim=64,
-                                     optim_type='adam',
-                                     lr=0.01)
+        self.net = baseline.Baseline(
+            in_channels=14,
+            seq_len=30,
+            num_layers=4,
+            kernel_size=3,
+            base_filters=16,
+            latent_dim=64,
+            optim_type="adam",
+            lr=0.01,
+        )
 
     @torch.no_grad()
     def test_encoder(self):
@@ -178,7 +191,9 @@ class TestBaseline(unittest.TestCase):
     def test_all_parameters_updated(self):
         optim = torch.optim.SGD(self.net.parameters(), lr=0.1)
 
-        loss = self.net.training_step((torch.randn(16, 14, 30), torch.ones(16)), batch_idx=0)
+        loss = self.net.training_step(
+            (torch.randn(16, 14, 30), torch.ones(16)), batch_idx=0
+        )
         loss.backward()
         optim.step()
 
@@ -186,7 +201,7 @@ class TestBaseline(unittest.TestCase):
             if param.requires_grad:
                 with self.subTest(name=param_name):
                     self.assertIsNotNone(param.grad)
-                    self.assertNotEqual(0., torch.sum(param.grad ** 2))
+                    self.assertNotEqual(0.0, torch.sum(param.grad ** 2))
 
     @torch.no_grad()
     def test_eval_metrics(self):
@@ -195,26 +210,30 @@ class TestBaseline(unittest.TestCase):
         target_labels = torch.ones(16)
 
         expected_prediction = self.net.regressor(self.net.encoder(target))
-        expected_loss = torch.sqrt(criterion(expected_prediction.squeeze(), target_labels))
-        actual_loss, _ = self.net._evaluate(target, target_labels)
+        expected_loss = torch.sqrt(
+            criterion(expected_prediction.squeeze(), target_labels)
+        )
+        actual_loss, _ = self.net.validation_step((target, target_labels), 0)
 
         self.assertEqual(expected_loss, actual_loss)
 
 
 class TestUnsupervisedPretraining(unittest.TestCase):
     def setUp(self):
-        self.net = pretraining.UnsupervisedPretraining(in_channels=14,
-                                                       seq_len=30,
-                                                       num_layers=4,
-                                                       kernel_size=3,
-                                                       base_filters=16,
-                                                       latent_dim=64,
-                                                       dropout=0.1,
-                                                       domain_tradeoff=0.001,
-                                                       domain_disc_dim=32,
-                                                       num_disc_layers=2,
-                                                       weight_decay=0,
-                                                       lr=0.01)
+        self.net = pretraining.UnsupervisedPretraining(
+            in_channels=14,
+            seq_len=30,
+            num_layers=4,
+            kernel_size=3,
+            base_filters=16,
+            latent_dim=64,
+            dropout=0.1,
+            domain_tradeoff=0.001,
+            domain_disc_dim=32,
+            num_disc_layers=2,
+            weight_decay=0,
+            lr=0.01,
+        )
 
     @torch.no_grad()
     def test_encoder(self):
@@ -226,28 +245,30 @@ class TestUnsupervisedPretraining(unittest.TestCase):
     def test_get_anchor_query_embeddings(self):
         anchors = torch.randn(16, 14, 30)
         queries = torch.randn(16, 14, 30)
-        anchor_embeddings, query_embeddings = self.net._get_anchor_query_embeddings(anchors, queries)
+        anchor_embeddings, query_embeddings = self.net._get_anchor_query_embeddings(
+            anchors, queries
+        )
 
         self.assertEqual(torch.Size((16, 64)), anchor_embeddings.shape)
         self.assertEqual(torch.Size((16, 64)), query_embeddings.shape)
 
         for norm in torch.norm(anchor_embeddings, dim=1).tolist():
-            self.assertAlmostEqual(1., norm, places=6)
+            self.assertAlmostEqual(1.0, norm, places=6)
         for norm in torch.norm(query_embeddings, dim=1).tolist():
-            self.assertAlmostEqual(1., norm, places=6)
+            self.assertAlmostEqual(1.0, norm, places=6)
 
     @torch.no_grad()
     def test_forward(self):
         anchors = torch.randn(16, 14, 30)
-        anchors[5] = 1.
+        anchors[5] = 1.0
         queries = torch.randn(16, 14, 30)
-        queries[5] = 1.
+        queries[5] = 1.0
         self.net.eval()
         distances = self.net(anchors, queries)
 
         self.assertEqual(torch.Size((16,)), distances.shape)
-        self.assertAlmostEqual(0., distances[5].item(), delta=1e-6)
-        self.assertNotAlmostEqual(0., distances.sum().item())
+        self.assertAlmostEqual(0.0, distances[5].item(), delta=1e-6)
+        self.assertNotAlmostEqual(0.0, distances.sum().item())
 
     def test_batch_independence(self):
         torch.autograd.set_detect_anomaly(True)
@@ -285,7 +306,12 @@ class TestUnsupervisedPretraining(unittest.TestCase):
     def test_all_parameters_updated(self):
         optim = torch.optim.SGD(self.net.parameters(), lr=0.1)
 
-        inputs = (torch.randn(16, 14, 30), torch.randn(16, 14, 30), torch.randn(16), torch.randn(16))
+        inputs = (
+            torch.randn(16, 14, 30),
+            torch.randn(16, 14, 30),
+            torch.randn(16),
+            torch.randn(16),
+        )
         loss = self.net.training_step(inputs, batch_idx=0)
         loss.backward()
         optim.step()
@@ -294,4 +320,4 @@ class TestUnsupervisedPretraining(unittest.TestCase):
             if param.requires_grad:
                 with self.subTest(name=param_name):
                     self.assertIsNotNone(param.grad)
-                    self.assertNotEqual(0., torch.sum(param.grad ** 2))
+                    self.assertNotEqual(0.0, torch.sum(param.grad ** 2))
