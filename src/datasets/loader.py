27d1c691d446e895a7735cb72a69c3f40714130a
Tilman Krokotsch <tilman.krokotsch@iav.de>
2021-03-05 14:03:16 +0100

    Add loader for FEMTO dataset.

--- src/datasets/loader.py
+++ src/datasets/loader.py
@@ -1,18 +1,53 @@
 import os
+import pickle
 import warnings
 from typing import List, Tuple
 
 import numpy as np
+import re
 import sklearn.preprocessing as scalers
 import torch
+from tqdm import tqdm
 
 
 class AbstractLoader:
+    def __init__(self, percent_fail_runs, percent_broken):
+        self.percent_fail_runs = percent_fail_runs
+        self.percent_broken = percent_broken
+
     def prepare_data(self):
         raise NotImplementedError
 
     def load_split(self, split: str) -> Tuple[List[torch.Tensor], List[torch.Tensor]]:
         raise NotImplementedError
+
+    def _truncate_runs(
+        self, features: List[np.ndarray], targets: List[np.ndarray]
+    ) -> Tuple[List[np.ndarray], List[np.ndarray]]:
+        # Truncate the number of runs to failure
+        if self.percent_fail_runs is not None and self.percent_fail_runs < 1:
+            num_runs = int(self.percent_fail_runs * len(features))
+            features = features[:num_runs]
+            targets = targets[:num_runs]
+
+        # Truncate the number of samples per run, starting at failure
+        if self.percent_broken is not None and self.percent_broken < 1:
+            for i, run in enumerate(features):
+                num_cycles = int(self.percent_broken * len(run))
+                features[i] = run[:num_cycles]
+                targets[i] = targets[i][:num_cycles]
+
+        return features, targets
+
+    def _to_tensor(
+        self, features: List[np.ndarray], targets: List[np.ndarray]
+    ) -> Tuple[List[torch.Tensor], List[torch.Tensor]]:
+        features = [
+            torch.tensor(f, dtype=torch.float32).permute(0, 2, 1) for f in features
+        ]
+        targets = [torch.tensor(t, dtype=torch.float32) for t in targets]
+
+        return features, targets
 
 
 class CMAPSSLoader(AbstractLoader):
@@ -34,6 +69,8 @@ class CMAPSSLoader(AbstractLoader):
         feature_select: List[int] = None,
         truncate_val: bool = False,
     ):
+        super().__init__(percent_fail_runs, percent_broken)
+
         # Select features according to https://doi.org/10.1016/j.ress.2017.11.021
         if feature_select is None:
             feature_select = self.DEFAULT_CHANNELS
@@ -41,8 +78,6 @@ class CMAPSSLoader(AbstractLoader):
         self.fd = fd
         self.window_size = window_size or self.WINDOW_SIZES[self.fd]
         self.max_rul = max_rul
-        self.percent_broken = percent_broken
-        self.percent_fail_runs = percent_fail_runs
         self.feature_select = feature_select
         self.truncate_val = truncate_val
 
@@ -122,24 +157,6 @@ class CMAPSSLoader(AbstractLoader):
 
         return features
 
-    def _truncate_runs(
-        self, features: List[np.ndarray], targets: List[np.ndarray]
-    ) -> Tuple[List[np.ndarray], List[np.ndarray]]:
-        # Truncate the number of runs to failure
-        if self.percent_fail_runs is not None and self.percent_fail_runs < 1:
-            num_runs = int(self.percent_fail_runs * len(features))
-            features = features[:num_runs]
-            targets = targets[:num_runs]
-
-        # Truncate the number of samples per run, starting at failure
-        if self.percent_broken is not None and self.percent_broken < 1:
-            for i, run in enumerate(features):
-                num_cycles = int(self.percent_broken * len(run))
-                features[i] = run[:num_cycles]
-                targets[i] = targets[i][:num_cycles]
-
-        return features, targets
-
     def _normalize(self, features: List[np.ndarray]) -> List[np.ndarray]:
         """Normalize features with sklearn transform."""
         # Fit scaler on corresponding training split
@@ -214,12 +231,187 @@ class CMAPSSLoader(AbstractLoader):
 
         return cropped_features
 
-    def _to_tensor(
-        self, features: List[np.ndarray], targets: List[np.ndarray]
-    ) -> Tuple[List[torch.Tensor], List[torch.Tensor]]:
-        features = [
-            torch.tensor(f, dtype=torch.float32).permute(0, 2, 1) for f in features
-        ]
-        targets = [torch.tensor(t, dtype=torch.float32) for t in targets]
+
+class FEMTOLoader(AbstractLoader):
+    DATA_ROOT = os.path.normpath(
+        os.path.join(os.path.dirname(__file__), "..", "..", "data", "FEMTOBearingDataSet")
+    )
+
+    def __init__(
+        self,
+        fd: int,
+        window_size: int = None,
+        max_rul: int = 125,
+        percent_broken: float = None,
+        percent_fail_runs: float = None,
+        feature_select: List[int] = None,
+        truncate_val: bool = False,
+    ):
+        super().__init__(percent_fail_runs, percent_broken)
+
+        self.fd = fd
+        self.window_size = window_size or FEMTOPreparator.DEFAULT_WINDOW_SIZE
+        self.max_rul = max_rul
+        self.feature_select = feature_select
+        self.truncate_val = truncate_val
+
+        self._preparator = FEMTOPreparator(self.fd, self.DATA_ROOT)
+
+    def prepare_data(self):
+        self._preparator.prepare_split("train")
+        self._preparator.prepare_split("test")
+
+    def load_split(self, split: str) -> Tuple[List[torch.Tensor], List[torch.Tensor]]:
+        features, targets = self._load_runs(split)
+        if split == "train":
+            features, targets = self._truncate_runs(features, targets)
+        features = self._scale_features(features)
+        features, targets = self._to_tensor(features, targets)
 
         return features, targets
+
+    def _load_runs(self, split: str):
+        features, targets = self._preparator.load_runs(split)
+        features = [f[:, -self.window_size :, :] for f in features]
+
+        return features, targets
+
+    def _scale_features(self, runs: List[np.ndarray]) -> List[np.ndarray]:
+        scaler = self._preparator.load_scaler()
+        for i, run in enumerate(runs):
+            run = run.reshape(-1, 2)
+            run = scaler.transform(run)
+            runs[i] = run.reshape(-1, self.window_size, 2)
+
+        return runs
+
+
+class FEMTOPreparator:
+    DEFAULT_WINDOW_SIZE = 2560
+    SPLIT_FOLDERS = {"train": "Learning_set", "test": "Full_Test_Set"}
+
+    def __init__(self, fd, data_root):
+        self.fd = fd
+        self._data_root = data_root
+
+    def prepare_split(self, split: str):
+        if not os.path.exists(self._get_run_file_path(split)):
+            print(f"Prepare FEMTO {split} data of condition {self.fd}...")
+            features, targets = self._load_raw_runs(split)
+            self._save_efficient(split, features, targets)
+        if split == "train" and not os.path.exists(self._get_scaler_path()):
+            features, _ = self.load_runs(split)
+            scaler = self._fit_scaler(features)
+            self._save_scaler(scaler)
+
+    def load_runs(self, split: str) -> Tuple[List[np.ndarray], List[np.ndarray]]:
+        save_path = self._get_run_file_path(split)
+        with open(save_path, mode="rb") as f:
+            features, targets = pickle.load(f)
+
+        return features, targets
+
+    def _load_raw_runs(self, split: str) -> Tuple[List[np.ndarray], List[np.ndarray]]:
+        file_paths = self._get_csv_file_paths(split)
+        features = self._load_raw_features(file_paths)
+        targets = self._targets_from_file_paths(file_paths)
+
+        return features, targets
+
+    def _load_raw_features(self, file_paths: List[List[str]]) -> List[np.ndarray]:
+        runs = []
+        for run_files in tqdm(file_paths, desc="Runs"):
+            run_features = np.empty((len(run_files), self.DEFAULT_WINDOW_SIZE, 2))
+            for i, file_path in enumerate(tqdm(run_files, desc="Files")):
+                run_features[i] = self._load_feature_file(file_path)
+            runs.append(run_features)
+
+        return runs
+
+    def _get_csv_file_paths(self, split: str) -> List[List[str]]:
+        split_path = self._get_split_folder(split)
+        run_folders = self._get_run_folders(split_path)
+        file_paths = []
+        for run_folder in run_folders:
+            run_path = os.path.join(split_path, run_folder)
+            feature_files = self._get_csv_files_in_path(run_path)
+            file_paths.append(feature_files)
+
+        return file_paths
+
+    def _get_split_folder(self, split: str) -> str:
+        return os.path.join(self._data_root, self.SPLIT_FOLDERS[split])
+
+    def _get_run_folders(self, split_path):
+        folder_pattern = self._get_run_folder_pattern()
+        all_folders = os.listdir(split_path)
+        run_folders = [f for f in all_folders if folder_pattern.match(f) is not None]
+
+        return run_folders
+
+    def _get_run_folder_pattern(self) -> re.Pattern:
+        return re.compile(fr"Bearing{self.fd}_\d")
+
+    def _get_csv_files_in_path(self, run_path):
+        feature_files = [f for f in os.listdir(run_path) if f.startswith("acc")]
+        feature_files = sorted(os.path.join(run_path, f) for f in feature_files)
+
+        return feature_files
+
+    def _load_feature_file(self, file_path: str) -> np.ndarray:
+        features = np.loadtxt(file_path, delimiter=",")
+        features = features[:, [4, 5]]
+
+        return features
+
+    def _targets_from_file_paths(self, file_paths: List[List[str]]) -> List[np.ndarray]:
+        targets = []
+        for run_files in file_paths:
+            run_targets = np.empty(len(run_files))
+            for i, file_path in enumerate(run_files):
+                run_targets[i] = self._timestep_from_file_path(file_path)
+            run_targets = run_targets[::-1].copy()
+            targets.append(run_targets)
+
+        return targets
+
+    def _timestep_from_file_path(self, file_path: str) -> int:
+        file_name = os.path.basename(file_path)
+        time_step = int(file_name[4:9])
+
+        return time_step
+
+    def _fit_scaler(self, features):
+        scaler = scalers.StandardScaler()
+        for run in features:
+            run = run.reshape(-1, run.shape[-1])
+            scaler.partial_fit(run)
+
+        return scaler
+
+    def _save_scaler(self, scaler):
+        save_path = self._get_scaler_path()
+        with open(save_path, mode="wb") as f:
+            pickle.dump(scaler, f)
+
+    def load_scaler(self) -> scalers.StandardScaler:
+        save_path = self._get_scaler_path()
+        with open(save_path, mode="rb") as f:
+            scaler = pickle.load(f)
+
+        return scaler
+
+    def _get_scaler_path(self):
+        return os.path.join(self._get_split_folder("train"), f"scaler_{self.fd}.pkl")
+
+    def _save_efficient(
+        self, split: str, features: List[np.ndarray], targets: List[np.ndarray]
+    ):
+        with open(self._get_run_file_path(split), mode="wb") as f:
+            pickle.dump((features, targets), f)
+
+    def _get_run_file_path(self, split: str) -> str:
+        split_folder = self._get_split_folder(split)
+        run_file_path = os.path.join(split_folder, f"runs_{self.fd}.pkl")
+
+        return run_file_path
