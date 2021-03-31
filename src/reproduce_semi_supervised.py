bd1f73fedf6693a1a5eb4d685a661adde90401e2
Tilman Krokotsch <tilman.krokotsch@iav.de>
2021-03-31 15:46:39 +0200

    Add script to reproduce semi-supervised_experiments.

---
+++ src/reproduce_semi_supervised.py
@@ -0,0 +1,73 @@
+import os
+from datetime import datetime
+import random
+
+from run_semi_supervised import run
+
+
+script_path = os.path.dirname(__file__)
+config_root = os.path.join(script_path, "..", "configs")
+
+
+def reproduce(base_version, master_seed):
+    if base_version is None:
+        base_version = datetime.now().timestamp()
+    error_log = []
+    fds = [1, 2, 3, 4]
+    percent_fails = [1.0, 0.4, 0.2, 0.1, 0.02]
+    percent_broken = [0.8]
+    random.seed(master_seed)
+    seeds = [
+        [
+            [random.randint(0, 99999999) for _ in range(len(percent_fails))]
+            for _ in range(len(percent_broken))
+        ]
+        for _ in range(len(fds))
+    ]
+
+    for num_fd, fd in enumerate(fds):
+        for num_broken, broken in enumerate(percent_broken):
+            for num_fails, fails in enumerate(percent_fails):
+                seed = seeds[num_fd][num_broken][num_fails]
+                arch_config_path = os.path.join(config_root, f"baseline_fd{fd}.json")
+                pre_config_path = os.path.join(config_root, f"baseline_pre_fd{fd}.json")
+                try:
+                    run(
+                        fd,
+                        broken,
+                        fails,
+                        arch_config_path,
+                        pre_config_path,
+                        pretrain=True,
+                        mode="linear",
+                        record_embeddings=False,
+                        replications=10,
+                        gpu=[0],
+                        master_seed=seed,
+                        version=f"{base_version}_pretrained@{broken:.2f}@{fails:.2f}",
+                    )
+                except Exception as e:
+                    error_log.append(e)
+
+    if error_log:
+        for e in error_log:
+            print(type(e), e)
+    else:
+        print("Everything went well.")
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Reproduce all semi-supervised experiments."
+    )
+    parser.add_argument(
+        "base_version", default=None, help="common prefix for the version tag"
+    )
+    parser.add_argument(
+        "--seed", default=21, help="master seed used to produce all other seeds"
+    )
+    opt = parser.parse_args()
+
+    reproduce(opt.base_version, opt.seed)
